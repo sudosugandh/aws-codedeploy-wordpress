19745106b03d8c2599dac2929a2e9cca8fb68481
Sugandh Kumar <64632983+sugandhkumar01@users.noreply.github.com>
2022-09-08 10:00:25 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,5 @@
 
 #!/bin/bash
-sudo amazon-linux-extras install php7.4
-sudo yum install -y httpd mariadb-server php
+sudo apt -y install php7.4
+sudo apt-get install -y php7.4-cli php7.4-json php7.4-common php7.4-mysql php7.4-zip php7.4-gd php7.4-mbstring php7.4-curl php7.4-xml php7.4-bcmath
+sudo apt install -y apache2
